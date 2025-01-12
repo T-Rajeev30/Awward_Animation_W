9df95f76f0432e5e2af0ba991c4122f84ad7839c
T-Rajeev30 <rajeevtiwariunder100@gmail.com>
2025-01-12 17:49:57 +0530

    Made 70% of the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Hero from './Components/Hero'
+
 
 const App = () => {
   return (
-    <div className='h-[100vh] w-[100%] bg-orange-500 text-2xl' >App</div>
+  <>
+  <main  className='relative min-h-screen w-screen overflow-x-hidden'>
+    <Hero/>
+  </main>
+  </>
   )
 }
 
